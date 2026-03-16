8601c32e48973b1709dd66b2c585e48622679999
Rey <audrey.marshabilla.2409117@students.um.ac.id>
2026-03-16 09:19:15 +0700

    add baru
    ini nyoba lagi

--- MoMA.py
+++ MoMA.py
@@ -10,6 +10,18 @@ def index():
 def about():
     return render_template('about.html')
 
+@app.route('/art')
+def art():
+    return render_template('art.html')
+
+@app.route('/artist')
+def artist():
+    return render_template('artist.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
     
