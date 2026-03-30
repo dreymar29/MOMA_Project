eacc5b231b2c9f1b3ef91e76ce8135c13ef2e000
Rey <audrey.marshabilla.2409117@students.um.ac.id>
2026-03-30 20:57:48 +0700

    database
    bikin database

---
+++ templates/models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.orm import declarative_base
+
+db_url = "sqlite:///database.db"
+
+engine = create_engine(db_url)
+
+Base = declarative_base()
+
+class Art(Base):
+    __tablename__ = "art"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+Base.metadata.create_all(engine)
+
